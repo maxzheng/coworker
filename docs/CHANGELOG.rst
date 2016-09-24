faf14618d574d6984c3288e6a4cd078774be463b
Max Zheng <mzheng@linkedin.com>
2016-09-23 19:02:50 -0700

    Publish version 0.0.2

---
+++ docs/CHANGELOG.rst
@@ -0,0 +1,6 @@
+Version 0.0.2
+================================================================================
+
+* Initial checkin
+* Initial checkin
+* Initial commit
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='coworker',
-    version='0.0.1',
+    version='0.0.2',
 
     author='Max Zheng',
     author_email='maxzheng.os @t gmail.com',
